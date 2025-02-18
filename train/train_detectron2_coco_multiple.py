a1510e45123faf5152bbf20c6bdf7eadbb4d01a8
yaman-aljnadi <60922667+yaman-aljnadi@users.noreply.github.com>
2025-02-17 23:33:16 -0500

    Update train_detectron2_coco_multiple.py

--- train/train_detectron2_coco_multiple.py
+++ train/train_detectron2_coco_multiple.py
@@ -1,7 +1,6 @@
-# Add detectron2 to the Python path (optional, as the installation above already does this)
 import sys
 import os
-sys.path.insert(0, os.path.abspath('Y:/Michigan_Tech_Courses/ALI_Work/Detectron/detectron2'))
+sys.path.insert(0, os.path.abspath('detectron2'))
 
 import os
 from detectron2.engine import DefaultTrainer
@@ -18,10 +17,10 @@ datasets = ["ACDC", "AutoEnhancer", "BayesianRetinex", "ICSP", "Original",
 for dataset in datasets:
     print(f"Training on dataset: {dataset}")
 
-    train_json = f"annotations/{dataset}/train.json"
-    val_json = f"annotations/{dataset}/val.json"
-    train_images = f"images/{dataset}/train"
-    val_images = f"images/{dataset}/val"
+    train_json = f"Enhanced_RUOD_coco/{dataset}/annotations/train.json"
+    val_json = f"Enhanced_RUOD_coco/{dataset}/annotations/val.json"
+    train_images = f"Enhanced_RUOD_coco/{dataset}/images/train"
+    val_images = f"Enhanced_RUOD_coco/{dataset}/images/val"
 
     train_name = f"{dataset}_train"
     val_name = f"{dataset}_val"
@@ -29,22 +28,23 @@ for dataset in datasets:
     register_coco_instances(val_name, {}, val_json, val_images)
 
     cfg = get_cfg()
-    cfg.merge_from_file(get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"))
+    cfg.merge_from_file(get_config_file("COCO-Detection/retinanet_R_50_FPN_3x.yaml"))
     cfg.DATASETS.TRAIN = (train_name,)
     cfg.DATASETS.TEST = (val_name,)
     cfg.DATALOADER.NUM_WORKERS = 0
-    cfg.MODEL.WEIGHTS = "detectron2://COCO-Detection/faster_rcnn_R_50_FPN_3x/137849458/model_final_280758.pkl"
+    cfg.MODEL.WEIGHTS = "detectron2://COCO-Detection/retinanet_R_50_FPN_3x/190397829/model_final_5bd44e.pkl"
     cfg.SOLVER.IMS_PER_BATCH = 2
     cfg.SOLVER.BASE_LR = 0.0015
-    cfg.SOLVER.MAX_ITER = 1100  
+    cfg.SOLVER.MAX_ITER = 1100 
     cfg.SOLVER.WARMUP_ITERS = 500
     cfg.TEST.EVAL_PERIOD = 300 
     cfg.MODEL.DEVICE = "cuda"
 
     
-    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 4  
+    cfg.MODEL.RETINANET.NUM_CLASSES = 10  
 
-    cfg.OUTPUT_DIR = f"output/{dataset}"
+    
+    cfg.OUTPUT_DIR = f"output_retinanet/{dataset}"
     os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
 
     trainer = DefaultTrainer(cfg)
